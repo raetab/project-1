6f6ffa5319ecdc3b5927693af2426b77824a0ff5
raes Tab <catdog2744@gmail.com>
2019-04-09 03:13:05 +0000

    doing the code with ASCII

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+int main () {
 
-int main () (
+    char message[]="HELLOWORLD";
+    int i;
+    int key; 
+    
 
-// how to commit?
-return 0;
-)
+    for (i = 65; i < 91; i++) {
+        if (i >= 65 && i <= 90) { /* If the char is uppercase */
+            i+= key; /* add the key */
+            if (i > 90) i -= 26; /* if the char is higher than the highest uppercase char, sub 26 */
+            if (i < 65) i += 26; /* if the char is lower than the lowest uppercase char, add 26 */
+     
+        printf("%c %d"\n, i, key);
+
+
+
+	return 0;
+}
