1591622e3fcb9dff601a6168c31a4a8c397cbd30
raes Tab <catdog2744@gmail.com>
2019-04-09 03:40:49 +0000

    Trying to fix ASCII code ??????

--- main.c
+++ main.c
@@ -6,23 +6,20 @@ int main () {
     int key; 
     
 
-    for (i = 65; i < 91; i++) {
+for (i = 65; i < 123; i++) {
+            if (i >= 65 && i <= 90) { /* If the char is uppercase */
+            i += key; /* add the key */
+            if (i > 90) i -= 26; /* if the char is higher than the highest uppercase char, sub 26 */
+            if (i < 65) i += 26; /* if the char is lower than the lowest uppercase char, add 26 */
             
-        printf("%c %d"\n, i, key);
+        } else if (i >= 97 && i <= 122) { /* else if it's lowercase */
+            i += key; /* add the key */
+            if (i > 122) i -= 26; /* if the char is higher than the highest lowercase char, sub 26 */
+            if (i < 97) i += 26; /* if the char is lower than the lowest lowercase char, add 26 */
+       }
 
-
-for (i = 0; i < 123; i++) {
-            if (ch >= 65 && ch <= 90) { /* If the char is uppercase */
-            ch += key; /* add the key */
-            if (ch > 90) ch -= 26; /* if the char is higher than the highest uppercase char, sub 26 */
-            if (ch < 65) ch += 26; /* if the char is lower than the lowest uppercase char, add 26 */
-            src[i] = (char)ch; /* set the current char in src to the char value of ch */
-        } else if (ch >= 97 && ch <= 122) { /* else if it's lowercase */
-            ch += key; /* add the key */
-            if (ch > 122) ch -= 26; /* if the char is higher than the highest lowercase char, sub 26 */
-            if (ch < 97) ch += 26; /* if the char is lower than the lowest lowercase char, add 26 */
-            src[i] = (char)ch;
-            
+          printf("%c %d\n", i, key);
             
 	return 0;
+}
 }
